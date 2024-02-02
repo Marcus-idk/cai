6be6457125fddcd1f2f8b3f850ef2beee892179c
caipng <caipng@gmail.com>
2024-02-02 11:01:19 +0000

    deploy app

--- client/src/utils/fetchAPI.js
+++ client/src/utils/fetchAPI.js
@@ -1,5 +1,5 @@
-const BACKEND_IP = "http://localhost:5000";
-// const BACKEND_IP = "http://3.91.96.90:5000";
+// const BACKEND_IP = "http://localhost:5000";
+const BACKEND_IP = "http://ec2-18-208-234-122.compute-1.amazonaws.com:5000";
 
 export async function fetchAPI(URL, body) {
   return await fetch(BACKEND_IP + URL, body);
